e0efcb4c31901f8780e713a5b3aac67afd967a25
zaid-kh <z.kh2800@gmail.com>
2023-11-07 14:42:45 +0200

    find method

--- 24.3/app.js
+++ 24.3/app.js
@@ -1,5 +1,4 @@
 Array.prototype.filterZaid = function (callbackFunction) {
-  console.log("zaid's filter function");
   const newArray = [];
   for (let i = 0; i < this.length; i++) {
     // if callback returns true -> push to newArray[]
@@ -18,4 +17,24 @@ console.log(
 console.log(
   "[23, 66, 98, 5, 13].filterZaid((item) => item % 2 === 0): ",
   [23, 66, 98, 5, 13].filterZaid((item) => item % 2 === 0)
+);
+
+Array.prototype.findZaid = function (callbackFunction) {
+  for (let i = 0; i < this.length; i++) {
+    // if callback returns true -> push to newArray[]
+    if (callbackFunction(this[i], i, this)) {
+      return this[i];
+    }
+  }
+  console.error("could not find what you are looking for");
+};
+
+const names = ["zaid", "ahmad", "mohammad", "ali"];
+console.log(
+  'names.findZaid("ali"): ',
+  names.findZaid((name) => name === "ali")
+);
+console.log(
+  'names.findZaid("e"): ',
+  names.findZaid((name) => name === "e")
 );
